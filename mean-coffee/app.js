75b96e6264a903c142f03a5ca4ccd730742bf4d6
Travis Shields <travis.shields@gmail.com>
2016-05-30 19:54:43 -0400

    starts REST routes

--- mean-coffee/app.js
+++ mean-coffee/app.js
@@ -5,16 +5,16 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// MongoDB
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/news');
+require('./models/Posts');
+require('./models/Comments');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
-var mongoose = require('mongoose');
-require('./models/Posts');
-require('./models/Comments');
-
-mongoose.connect('mongodb://localhost/news');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
